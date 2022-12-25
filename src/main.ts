b2054a5fd8d9c445f14b4240ab58b2aa02288fce
pino._.hoo <inhoo23@naver.com>
2022-12-25 12:18:10 +0900

    Init : Create Project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  await app.listen(process.env.NODE_SERVER_PORT || 3009);
+}
+bootstrap();
